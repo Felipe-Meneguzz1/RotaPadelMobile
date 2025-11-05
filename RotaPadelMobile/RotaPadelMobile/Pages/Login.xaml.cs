4710693a2b6696b9c2b33d3c9deb2c40a029c575
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-11-04 23:23:32 -0300

    Feat: Criação do Front da tela de login

--- RotaPadelMobile/RotaPadelMobile/Pages/Login.xaml.cs
+++ RotaPadelMobile/RotaPadelMobile/Pages/Login.xaml.cs
@@ -1,9 +1,28 @@
-namespace RotaPadelMobile.Pages;
-
-public partial class Login : ContentPage
+namespace RotaPadelMobile.Pages
 {
-	public Login()
-	{
-		InitializeComponent();
-	}
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnEntrarClicked(object sender, EventArgs e)
+        {
+
+            await DisplayAlert("Login", "Funcionalidade em desenvolvimento", "OK");
+        }
+
+        private async void OnEsqueciSenhaClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Esqueci minha senha", "Funcionalidade em desenvolvimento", "OK");
+        }
+
+        private async void OnCadastroClicked(object sender, EventArgs e)
+        {
+            // Volta para tela inicial e abre cadastro
+            await Navigation.PopModalAsync();
+            await Navigation.PushModalAsync(new Cadastro());
+        }
+    }
 }
