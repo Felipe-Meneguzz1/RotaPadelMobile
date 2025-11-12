dc64278359d24566381f438ecf030b891421e2aa
AndersonCaniniD <166625747+AndersonCagniniD@users.noreply.github.com>
2025-11-12 08:40:48 -0300

    Feat:UpdateUsuário
    Front do update usuário

---
+++ RotaPadelMobile/RotaPadelMobile/Pages/UpdatePerfil.xaml.cs
@@ -0,0 +1,29 @@
+namespace RotaPadelMobile.Pages;
+
+public partial class UpdatePerfil : ContentPage
+{
+	public UpdatePerfil()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_3(object sender, EventArgs e)
+    {
+
+    }
+}
